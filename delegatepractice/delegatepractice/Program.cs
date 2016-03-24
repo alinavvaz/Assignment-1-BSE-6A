2b61cdb77512455883f3978be31db5a5d062c91b
Ali <alixm50@yahoo.com>
2016-03-24 22:46:37 +0500

    delegate understanding 1

---
+++ delegatepractice/delegatepractice/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+delegate int Changer(int n);
+namespace delegatepractice
+{
+    class Program
+    {
+        static int num = 2;
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+
+        public static int MultiplyNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+
+        static void Main(string[] args)
+        {
+            Changer nc1 = new Changer(AddNum);
+            Changer nc2 = new Changer(MultiplyNum);
+
+            nc1(25);
+            Console.WriteLine("Value of Num1=", getNum());
+            nc2(5);
+            Console.WriteLine("Value of Num2=", getNum());
+            Console.ReadKey();
+        }
+    }
+}
